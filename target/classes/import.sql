30ef775dc9f3f86761c86fbe894f60318058510e
PATRICIA PARK <pyjee8@google.com>
2025-04-24 23:48:31 -0600

    ✅ Include JAR for Render deployment

---
+++ target/classes/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO orders (id, item, status) VALUES (1, 'Brick', 'RECEIVED');
+INSERT INTO orders (id, item, status) VALUES (2, 'Laser', 'SHIPPING');
+INSERT INTO orders (id, item, status) VALUES (3, 'Rocket', 'COMPLETED');
