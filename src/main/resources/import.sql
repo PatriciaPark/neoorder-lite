2930af07a8b2a18a87479b2ad9ae996866e3e8f7
PATRICIA PARK <pyjee8@google.com>
2025-05-12 19:55:44 -0600

    edit sql4

--- src/main/resources/import.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO orders (id, item, status) VALUES (1, 'Brick', 'RECEIVED');
-INSERT INTO orders (id, item, status) VALUES (2, 'Laser', 'SHIPPING');
-INSERT INTO orders (id, item, status) VALUES (3, 'Rocket', 'COMPLETED');
